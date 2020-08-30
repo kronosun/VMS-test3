9a477ad1f080145097a592474226c511814f192e
davidf1000 <47879766+davidf1000@users.noreply.github.com>
2020-08-30 21:23:37 +0700

    little upd

--- src/components/dashboard/layout/SidePanel.js
+++ src/components/dashboard/layout/SidePanel.js
@@ -50,7 +50,7 @@ const SidePanel = ({msg}) => {
             <i style={{fontSize:'2rem'}} className="fas fa-fw fa-user-friends"></i>
             <br />
 
-            {msg==='visitor'?<u style={{fontSize:'0.8rem'}}>Visitor Number</u> : <span style={{fontSize:'0.8rem'}}>Visitor Number</span> }
+            {msg==='visitor'?<u style={{fontSize:'0.8rem'}}>Ward Livestream</u> : <span style={{fontSize:'0.8rem'}}>Visitor Number</span> }
 
           </Link>
 
@@ -61,7 +61,7 @@ const SidePanel = ({msg}) => {
             <i style={{fontSize:'2rem'}} className="fas fa-fw fa-bed"></i>
             <br />
 
-            {msg==='livestream'?<u style={{fontSize:'0.8rem'}}>Livestream</u> : <span style={{fontSize:'0.8rem'}}>Livestream</span> }
+            {msg==='livestream'?<u style={{fontSize:'0.8rem'}}>Bed Livestream</u> : <span style={{fontSize:'0.8rem'}}>Livestream</span> }
 
           </Link>
         </li>
