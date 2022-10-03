d9f082ddb1081a41f81dd1eafe8b1bdecbd43398
Thomas Vetea <thomas@kronosun.com>
2022-10-03 19:14:56 +0400

    Register with AWS Cognito

--- src/UserPool.js
+++ src/UserPool.js
@@ -2,7 +2,7 @@ import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
   UserPoolId: process.env.USER_POOL_ID || "eu-west-3_Y15EY2p3g",
-  ClientId: process.env.CLIENT_ID || "or9ndlmo0kth4qv551h062cs",
+  ClientId: process.env.CLIENT_ID || "22beftv0oqmk9qgs4ik3fncoq9",
 };
 
 const UserPool = new CognitoUserPool(poolData);
