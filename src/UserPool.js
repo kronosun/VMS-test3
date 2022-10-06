4389a65817eda0bcf0658f687dbd182fb63a6c6b
Thomas Vetea <thomas@kronosun.com>
2022-10-06 09:22:10 +0400

    New Group id for AWS Cognito

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-  UserPoolId: process.env.USER_POOL_ID || "eu-west-3_Y15EY2p3g",
-  ClientId: process.env.CLIENT_ID || "22beftv0oqmk9qgs4ik3fncoq9",
+  UserPoolId: process.env.USER_POOL_ID || "eu-west-3_P6XK74lL5",
+  ClientId: process.env.CLIENT_ID || "73805ovqp2a54h8utuogpnj9ni",
 };
 
 const UserPool = new CognitoUserPool(poolData);
